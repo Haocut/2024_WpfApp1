f3f025cea434ffbd21e1468d5c56be5ff01673e8
Haocut <4b2g0126@office.stust.edu.tw>
2024-10-30 02:05:15 +0800

    重構程式碼並新增 Drink 類別
    在 `Program.cs` 中，移除原本要求使用者輸入數字並顯示乘法表的程式碼，改為建立一個包含飲料資訊的 `Drink` 類別列表，並在迴圈中列印這些飲料的資訊。
    在 `Drinks.cs` 中，新增 `Drink` 類別，包含 `Name`、`Size` 和 `Price` 三個屬性。

---
+++ 2024wpfapp1/Drinks.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024wpfapp1
+{
+    internal class Drink
+    {
+        public String Name { get; set; }
+        public String Size { get; set; }
+        public int Price { get; set; }
+
+    }
+}
